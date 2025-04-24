8117969932777c5ed3e9466fcca067bd1f36741e
fadliaditya06 <fadliadtyas10@gmail.com>
2025-04-24 10:33:05 +0700

    Models : Permintaan Konfirmasi for dummy data

---
+++ lib/models/permintaan_konfirmasi_model.dart
@@ -0,0 +1,68 @@
+class PermintaanKonfirmasiModel {
+  final String id;
+  final String name; 
+  final String status; 
+  final String lokasi; 
+  final String jenisPermintaan; 
+  final DateTime tanggalPermintaan;
+
+  PermintaanKonfirmasiModel({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.lokasi,
+    required this.jenisPermintaan,
+    required this.tanggalPermintaan,
+  });
+
+  static List<PermintaanKonfirmasiModel> dummyDataKonfirmasi = [
+    PermintaanKonfirmasiModel(
+      id: '1',
+      name: 'Hana Annisa',
+      status: 'Permintaan Disetujui',
+      lokasi: 'One Mall Batam',
+      jenisPermintaan: 'Katalog Lokal',
+      tanggalPermintaan: DateTime(2024, 4, 10),
+    ),
+    PermintaanKonfirmasiModel(
+      id: '2',
+      name: 'Hana Annisa',
+      status: 'Permintaan Disetujui',
+      lokasi: 'One Mall Batam',
+      jenisPermintaan: 'Katalog Lokal',
+      tanggalPermintaan: DateTime(2024, 4, 15),
+    ),
+    PermintaanKonfirmasiModel(
+      id: '3',
+      name: 'Hana Annisa',
+      status: 'Permintaan Disetujui',
+      lokasi: 'One Mall Batam',
+      jenisPermintaan: 'Katalog Lokal',
+      tanggalPermintaan: DateTime(2024, 4, 9),
+    ),
+    PermintaanKonfirmasiModel(
+      id: '4',
+      name: 'Hana Annisa',
+      status: 'Permintaan konfirmasi anda ditolak karena beberapa alasan',
+      lokasi: 'One Mall Batam',
+      jenisPermintaan: 'Katalog Lokal',
+      tanggalPermintaan: DateTime(2024, 4, 23),
+    ),
+    PermintaanKonfirmasiModel(
+      id: '5',
+      name: 'Hana Annisa',
+      status: 'Permintaan konfirmasi anda ditolak karena beberapa alasan',
+      lokasi: 'One Mall Batam',
+      jenisPermintaan: 'Katalog Lokal',
+      tanggalPermintaan: DateTime(2024, 4, 25),
+    ),
+    PermintaanKonfirmasiModel(
+      id: '6',
+      name: 'Hana Annisa',
+      status: 'Permintaan konfirmasi anda ditolak karena beberapa alasan',
+      lokasi: 'One Mall Batam',
+      jenisPermintaan: 'Katalog Lokal',
+      tanggalPermintaan: DateTime(2024, 4, 28),
+    ),
+  ];
+}
