715bc6dd5f5f33c8f0c112a0fd9ad2b268d6349e
fadliaditya06 <fadliadtyas10@gmail.com>
2025-07-18 20:02:05 +0700

    Small change in load test tambah data

--- lib/performance/load_test_tambah_data.dart
+++ lib/performance/load_test_tambah_data.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:geoportal_mobile/controllers/tambah_data_controller.dart';
 import 'package:flutter/services.dart' show rootBundle;
